fde33e972cb4b7bf654ed130760a8a94cecddf1b
Matthew Severance <MSSevs1313@gmail.com>
2016-11-13 16:35:58 -0500

    Small cleanups- more in order File name fixed for windows SpriteTest functionality wrapped in sprite_test() for Justin's Testing

--- SpriteTest.py
+++ SpriteTest.py
@@ -2,23 +2,26 @@ import pygame
 import TiledMap
 from Sprite import SpritePOC
 
-pygame.init()
+def sprite_test():
+    pygame.init()
 
-screen = pygame.display.set_mode((800, 800))
+    screen = pygame.display.set_mode((800, 800))
 
-world_map = TiledMap.WorldMap("Map2")
+    world_map = TiledMap.WorldMap("Map2")
 
-world_map.render_entire_map()
+    world_map.render_entire_map()
 
-sprite = SpritePOC.Sprite(world_map)
-s2 = SpritePOC.Sprite(world_map)
+    sprite = SpritePOC.Sprite(world_map)
+    s2 = SpritePOC.Sprite(world_map)
 
-sprite.spawn(screen)
-s2.spawn(screen)
-pygame.display.update()
-
-while True:
-    pygame.time.delay(1000)
-    sprite.move(world_map, screen)
-    s2.move(world_map, screen)
+    sprite.spawn(screen)
+    s2.spawn(screen)
     pygame.display.update()
+
+    while True:
+        pygame.time.delay(1000)
+        sprite.move(world_map, screen)
+        s2.move(world_map, screen)
+        pygame.display.update()
+
+sprite_test()
